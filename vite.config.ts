6ec61efef284fcb1db3263397d578b1055e38236
Sistema de Mandados <sistema-mandados@pcsp.gov.br>
2026-02-24 20:03:51 -0300

    feat: Atualiza o ícone do aplicativo e os assets do PWA para `novo_brasao_lock.png` em `vite.config.ts` e `index.html`.

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
       react(),
       VitePWA({
         registerType: 'autoUpdate',
-        includeAssets: ['brasao_police_siren.png', 'brasao_pcsp_colorido.png', 'brasao_pcsp_nova.png'],
+        includeAssets: ['novo_brasao_lock.png', 'brasao_pcsp_colorido.png', 'brasao_pcsp_nova.png'],
         manifest: {
           name: 'Sistema de Mandados PCSP',
           short_name: 'Mandados',
@@ -23,12 +23,12 @@ export default defineConfig(({ mode }) => {
           start_url: '/',
           icons: [
             {
-              src: 'brasao_police_siren.png',
+              src: 'novo_brasao_lock.png',
               sizes: '192x192',
               type: 'image/png'
             },
             {
-              src: 'brasao_police_siren.png',
+              src: 'novo_brasao_lock.png',
               sizes: '512x512',
               type: 'image/png'
             }
